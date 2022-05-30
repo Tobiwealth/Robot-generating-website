9a272d814daf58c3a28edf0b63673e55878ebb97
Tobiwealth <101267703+Tobiwealth@users.noreply.github.com>
2022-05-30 21:59:44 +0100

    Updates

---
+++ precache-manifest.d2c97092a87f6beb20b4427e0e57fa8c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "24a44e6d07bb84add409",
+    "url": "/Robot-generating-website/static/css/main.b4d03702.chunk.css"
+  },
+  {
+    "revision": "24a44e6d07bb84add409",
+    "url": "/Robot-generating-website/static/js/main.24a44e6d.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/Robot-generating-website/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "c2161ac0cffac15a61b4",
+    "url": "/Robot-generating-website/static/css/2.588271d2.chunk.css"
+  },
+  {
+    "revision": "c2161ac0cffac15a61b4",
+    "url": "/Robot-generating-website/static/js/2.c2161ac0.chunk.js"
+  },
+  {
+    "revision": "e4512980e0b2758193f7",
+    "url": "/Robot-generating-website/static/js/runtime~main.e4512980.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robot-generating-website/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "17523b0d65c9ea8f211049abb8e1bd65",
+    "url": "/Robot-generating-website/index.html"
+  }
+];
